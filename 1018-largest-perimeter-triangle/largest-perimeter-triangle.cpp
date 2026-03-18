3a805bd5df641321347813da17bd375a4913b5a3
harshitmishra00182-ops <harshitmishra00182@gmail.com>
2026-03-18 06:35:49 +0530

    Time: 18 ms (8.34%) | Memory: 25.7 MB (13.39%) - LeetSync

---
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        //OR
+        // sort(nums.begin(), nums.end());
+        // reverse(nums.begin(), nums.end());
+        for(int i = 0 ; i < nums.size()-2 ; i++){
+            if(nums[i+1]+nums[i+2]>nums[i]){
+                return nums[i+1]+nums[i+2]+nums[i];
+            }
+        }
+        return 0;
+    }
+};
